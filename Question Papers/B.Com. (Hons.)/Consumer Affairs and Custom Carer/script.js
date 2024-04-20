6660b59740c4fd9a37c921d89e4760ee90d8bec5
Dhruv Badaya <dhruv.badaya@gmail.com>
2024-04-20 13:01:01 +0530

    Update script.js

--- Question Papers/B.Com. (Hons.)/Consumer Affairs and Custom Carer/script.js
+++ Question Papers/B.Com. (Hons.)/Consumer Affairs and Custom Carer/script.js
@@ -1,20 +1,20 @@
 const buttonLinks = {
-    "2010": "2010.pdf",
-    "2011": "2011.pdf",
-    "2012": "2012.pdf",
-    "2013": "2013.pdf",
-    "2014": "2014.pdf",
-    "2015": "2015.pdf",
-    "2016": "2016.pdf",
-    "2017": "2017.pdf",
-    "2018": "2018.pdf",
-    "2019": "2019.pdf",
-    "2020": "2020.pdf",
-    "2021": "2021.pdf",
-    "2022": "2022.pdf",
-    "2023": "2023.pdf"
+    "2023": `${urlString}/${courseString}/${subjectString}/2023.pdf`,
+    "2022": `${urlString}/${courseString}/${subjectString}/2022.pdf`,
+    "2021": `${urlString}/${courseString}/${subjectString}/2021.pdf`,
+     "2020": `${urlString}/${courseString}/${subjectString}/2020.pdf`,
+     "2019": `${urlString}/${courseString}/${subjectString}/2019.pdf`,
+     "2018": `${urlString}/${courseString}/${subjectString}/2018.pdf`,
+     "2017": `${urlString}/${courseString}/${subjectString}/2017.pdf`,
+     "2016": `${urlString}/${courseString}/${subjectString}/2016.pdf`,
+     "2015": `${urlString}/${courseString}/${subjectString}/2015.pdf`,
+     "2014": `${urlString}/${courseString}/${subjectString}/2014.pdf`,
     // Add more buttons and links here as needed
 };
+
+var urlString = "https://docs.google.com/gview?embedded=true&url=https://www.dhruvbadaya.in/CourseCode/Question%20Papers";
+var courseString = "B.Com.%20(Hons.)"
+var subjectString = "Consumer%20Affairs%20and%20Custom%20Carer"
 
 const buttonsContainer = document.getElementById('buttons-container');
 
